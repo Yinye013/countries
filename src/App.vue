080976ecc05e016249dadea99d942ad1a2540538
Yinye013 <yinadesanya@gmail.com>
2022-10-12 23:55:16 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-midnight text-tahiti">
+    <h1 class="color-red-500">Hi!</h1>
+    <p class="">Happy to hqave you</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
